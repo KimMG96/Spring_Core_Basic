6015b79cfc528c24b453d0b59bf7e9e7be119bb0
KimMG96 <kim3jj@icloud.com>
2023-06-29 19:43:32 +0900

    회원 도메인 실행과 테스트

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //의존관계가 인터페이스 뿐만 아니라 구현까지 모두 의존하는 문제점이 있다.
 
     @Override
     public void join(Member member) {
